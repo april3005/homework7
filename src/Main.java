b2160362b4805df0c58f0bca87011143b8f96ffd
april3005 <55992572+april3005@users.noreply.github.com>
2019-10-19 23:43:15 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Кто вам нужен, man или woman?");
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
+        Woman woman = new Woman();
+        printInfo(woman);
+    }
 
-            printInfo(input);
+    private static void printInfo(A a) {
+        System.out.println(a.getAge() + " " + a.getWeight() + " " + a.getHeight());
+
+        a.method1(25, 55, 178);
+        a.method2(28, 48, 169);
+
 
 
     }
 
-    public static void printInfo(String obj) {
-
-            switch (obj) {
-                case "man":
-                    A m = new Man();
-                    System.out.println("Man");
-
-                    System.out.println(m.getAge());
-                    System.out.println(m.getWeight());
-                    System.out.println(m.getHeight());
-
-                    m.method1(20, 60, 172);
-                    m.method2(30, 80, 185);
-                    break;
-                case "woman":
-                    A w = new Woman();
-                    System.out.println("Woman");
-
-                    System.out.println(w.getAge());
-                    System.out.println(w.getWeight());
-                    System.out.println(w.getHeight());
-
-                    w.method1(25, 55, 178);
-                    w.method2(28, 48, 169);
-                    break;
-            }
     }
-}
